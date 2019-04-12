c1648c7f070cf4cbfa24d3d2aeb4205a253c145e
Simon Belbeoch <sbelbeoch@octo.com>
2019-04-12 18:07:54 +0200

    redirect http to https on production environement

--- server.js
+++ server.js
@@ -3,6 +3,17 @@ const serveStatic = require('serve-static')
 const path = require('path')
 
 const app = express()
+if (process.env.NODE_ENV === 'production') {
+  app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https') {
+      res.redirect(`https://${req.header('host')}${req.url}`)
+    }
+    else {
+      next()
+    }
+  })
+}
+
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, '/dist/index.html'))
