16fee4c64c62fdf35ff011aa7444913166d74063
Simon Belbeoch <sbelbeoch@octo.com>
2019-05-05 19:47:23 +0200

    better design for video list

--- src/views/Category.vue
+++ src/views/Category.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
-    <div v-for="video in videos" :key="video.oid">
+    <article v-for="video in videos" :key="video.oid">
       <router-link :to="{ name: 'video', params: { videoId: video.oid } }">
-        <div>{{ video.title }}</div>
-        <img :src="video.thumb" :alt="video.title">
+        <figure>
+          <img :src="video.thumb" :alt="video.title">
+          <figcaption>{{ video.title }}</figcaption>
+        </figure>
       </router-link>
-    </div>
+    </article>
   </div>
 </template>
 
@@ -26,4 +28,26 @@ export default {
 </script>
 
 <style scoped>
+  article {
+    display: inline-block;
+    position: relative;
+    margin: 30px;
+  }
+  figure {
+    display: inline-block;
+    margin: 0;
+  }
+  img {
+    width: 240px;
+  }
+  figcaption {
+    overflow: hidden;
+    position: absolute;
+    top: 15%;
+    padding: 5px 30px;
+    color: white;
+    background-color: rgba(1,1,1,0.8);
+    width: 180px;
+    text-align: center;
+  }
 </style>
